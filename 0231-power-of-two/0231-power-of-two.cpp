7a8f998e55236b695283b5872b408c1f7c6984f1
JAY BALDWA <91842833+JayBaldwa30@users.noreply.github.com>
2024-07-01 19:00:48 +0530

    Time: 2 ms (38.60%), Space: 7.2 MB (36.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        for(int i=0; i<=30; i++){
-            int ans=pow(2,i);
-            if(ans==n){
-                return true;
-            }
-        }
-        return false;
+        //naive way brute force
+        // for(int i=0; i<=30; i++){
+        //     int ans=pow(2,i);
+        //     if(ans==n){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        if(n<=0)return false;
+        return (n&(n-1))==0;
     }
 };
